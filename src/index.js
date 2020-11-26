e720f701ef8ea735a8fe736595e4f460486a3238
Kanetomo <52394458+Kanetomo@users.noreply.github.com>
2020-11-27 08:48:46 +0900

    スプレッド構文　map fileter

--- src/index.js
+++ src/index.js
@@ -80,3 +80,85 @@
 // }
 
 // console.log(func3(10, 20));
+
+// 分割代入
+// オブジェクトの分割代入
+// const myProfile = {
+// name: "じゃけぇ",
+// age: 28,
+// }
+
+// const{ name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// 配列の分割代入
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+// デフォルト値
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん!`);
+// sayHello();
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// 配列をまとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1, num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+// これは参照を別にする→コピー後の値を変更しても元の数字は変化しない
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+// 配列をループさせて何かしらの処理をするやり方と
+// nameArr.map((name) => console.log(name));
+
+// リターンされた結果に基づいて新しい配列を作成する
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+// console.log(nameArr2);
+
+// const numArr = [1, 2, 3 ,4 ,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+//よく使うやり方
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+/**
+ * 三項演算子
+ */
+// 　可読性の問題で多用してはならないが便利
+// 　ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
