0d9ef316eb2cadc5293c9b661c47d5563ecd9c6c
Kanetomo <52394458+Kanetomo@users.noreply.github.com>
2020-11-27 15:53:13 +0900

    参考演算子　論理演算子

--- src/index.js
+++ src/index.js
@@ -162,3 +162,37 @@
 // 　ある条件 ? 条件がtrueの時 : 条件がfalseの時
 // const val1 = 1 < 0 ? 'trueです' : 'falseです';
 // console.log(val1);
+
+// const num = '1300';
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString(): '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
